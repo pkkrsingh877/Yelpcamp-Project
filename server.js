82cbe4b24048761301a70ee64b878e0909c2b065
Prabhat Kumar <pkkrsingh877@gmail.com>
2022-02-27 05:12:37 +0530

    created a route for creating campgrounds

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = 3000;
 const path = require('path');
 const mongoose = require('mongoose');
+const Campground = require('./models/campground.js')
 
 // setting ejs up
 app.set('view engine', 'ejs');
@@ -16,6 +17,17 @@ try {
     console.log('DB CONNECTION FAILED!');
     console.log(err);
 }
+
+app.get('/makecampground', async (req, res) => {
+    const campground = new Campground({
+        title: 'Area 27 Stage 3',
+        price: '23',
+        description: 'Fight against picoolo, and get a chance to fight against Launch and Emperor Pilaf. You can get lots of green gems.',
+        location: 'Area 27 Stage 3'
+    });
+    await campground.save();
+    res.send(campground);
+});
 
 app.get('/', (req, res) => {
     res.render('home');
