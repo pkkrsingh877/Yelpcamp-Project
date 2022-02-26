557a8a95d4718dbf62a24678eae1ee4552e50a9a
Prabhat Kumar <pkkrsingh877@gmail.com>
2022-02-27 05:00:43 +0530

    added mongoose connection string and created campground model

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 3. Add .gitignore file to ignore node_modules
 4. Create basic express app
 5. Add ejs
+6. Add mongoose connection string 
+7. Create a campground model
--- server.js
+++ server.js
@@ -2,10 +2,20 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const path = require('path');
+const mongoose = require('mongoose');
 
 // setting ejs up
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+// setting mongoose up
+try {
+    mongoose.connect('mongodb://localhost:27017/yelpcamp');
+    console.log('DB CONNECTION SUCCESSFUL!');
+} catch (err) {
+    console.log('DB CONNECTION FAILED!');
+    console.log(err);
+}
 
 app.get('/', (req, res) => {
     res.render('home');
