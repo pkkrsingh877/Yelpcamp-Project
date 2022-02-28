8163c5225240284a5caab0636bc15b004e25c9f4
Prabhat Kumar <pkkrsingh877@gmail.com>
2022-02-28 05:57:15 +0530

    created show page and show route to show individual campgrounds

--- server.js
+++ server.js
@@ -18,6 +18,12 @@ try {
     console.log(err);
 }
 
+app.get('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render('campgrounds/show', { campground });
+});
+
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds });
