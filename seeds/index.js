946805940c89cb51f4a74a1c4a48ee10a973ac40
Prabhat Kumar <pkkrsingh877@gmail.com>
2022-03-02 22:07:27 +0530

    git random images from unsplash and save the url in database

--- seeds/index.js
+++ seeds/index.js
@@ -16,11 +16,12 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 1000; i++) {
-        const random1000 = Math.floor(Math.random() * 1000);
+    for (let i = 0; i < 25; i++) {
+        const random25 = Math.floor(Math.random() * 25);
         const camp = new Campground({
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            location: `${cities[random25].city}, ${cities[random25].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251'
         })
         console.log(`${i + 1}. ${cities[random1000].city}, ${cities[random1000].state}`);
         await camp.save();
